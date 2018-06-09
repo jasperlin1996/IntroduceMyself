d31c15f831e6ee62fe76ce38e55d7c6c85b8a438
jasperlin1996 <jasperlin1996@gmail.com>
2018-06-09 21:47:01 +0800

    優化js / 新增大頭貼 / 更改exp id

--- js/click.js
+++ js/click.js
@@ -3,89 +3,78 @@ $(function(){
     var animationIn = "animated fadeInLeft";
     var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";	/*webkit/moz/MS/opera/animationend*/
     /*emoticon change*/
-    function emoticonAnimate(pre, next){
+    function emoticonAnimate(pre, next, In, Out){
         $(pre).on('click',function() {
             console.log(pre)
-            $(this).addClass(animationOut).one(animationEnd, function(){
-                $(this).hide().removeClass(animationOut);
-                $(next).show().addClass(animationIn).one(animationEnd, function(){
-                    $(this).removeClass(animationIn);
+            $(this).addClass(Out).one(animationEnd, function(){
+                $(this).hide().removeClass(Out);
+                $(next).show().addClass(In).one(animationEnd, function(){
+                    $(this).removeClass(In);
                 });
             });
         });
     }
-    emoticonAnimate("#emoticon01", "#emoticon02");
-    emoticonAnimate("#emoticon02", "#emoticon03");
-    emoticonAnimate("#emoticon03", "#emoticon01");
+    emoticonAnimate("#emoticon01", "#emoticon02", animationIn, animationOut);
+    emoticonAnimate("#emoticon02", "#emoticon03", animationIn, animationOut);
+    emoticonAnimate("#emoticon03", "#emoticon01", animationIn, animationOut);
 
     
-    /* #emoticon, #about/ #name, #skills, #edu, #contact/#links */
+    /* #emoticon, #exp/ #name, #skills, #edu, #contact/#links */
     var animationOutLeft = "animated fadeOutLeft";
     var animationOutRight = "animated fadeOutRight";
     var animationZoomOut = "animated zoomOutUp";
     var animationInLeft = "animated fadeInLeft";
     var animationInRight = "animated fadeInRight";
 
-    var fontSize01 = parseInt($("#about").css("font-size"));
+    var fontSize01 = parseInt($("#exp").css("font-size"));
     fontSize01 = fontSize01 + 30 + "px";
-    var fontSize02 = parseInt($("#about").css("font-size"));
-    var about_status = 0;
+    var fontSize02 = parseInt($("#exp").css("font-size"));
     fontSize02 = fontSize02 + 0 + "px";
     
     var changingCertificate = 0;
 
-    function animateLeft(selectClass, show){                                            // IN
-        $(selectClass).show().addClass(animationInLeft).one(animationEnd, function(){
-            $(selectClass).removeClass(animationInLeft);
+    function recoverClass(selectClass, show, direction){                          // IN
+        $(selectClass).show().addClass(direction).one(animationEnd, function(){
+            $(selectClass).removeClass(direction);
             if(show === 1) $(selectClass).show();
         });	
     }
-    function animateRight(selectClass, show){
-        $(selectClass).show().addClass(animationInRight).one(animationEnd, function(){
-            $(selectClass).removeClass(animationInRight);
-            if(show === 1) $(selectClass).show();
+
+    function hideClass(selectClass, direction){
+        $(selectClass).addClass(direction).one(animationEnd, function(){         // OUT
+            $(selectClass).hide().removeClass(direction);
         });
     }
 
-    function hideClassLeft(selectClass){
-        $(selectClass).addClass(animationOutLeft).one(animationEnd, function(){         // OUT
-            $(selectClass).hide().removeClass(animationOutLeft);
-        });
-    }
-    function hideClassRight(selectClass){
-        $(selectClass).addClass(animationOutRight).one(animationEnd, function(){
-            $(selectClass).hide().removeClass(animationOutRight);
-        });
-    }
 
-    $( "#about").on('click', function(){											//col-md-6 col-lg-6  col-md-10 col-lg-10
-        hideClassLeft("#emoticon");
-        hideClassRight("#name");
-        hideClassLeft("#skills");
-        hideClassRight("#edu");
-        hideClassRight("#contact");
-        hideClassRight("#links");
-        $( "#about").addClass(animationZoomOut).one(animationEnd, function(){
+    $( "#exp").on('click', function(){											//col-md-6 col-lg-6  col-md-10 col-lg-10
+        hideClass("#emoticon", animationOutLeft);
+        hideClass("#name", animationOutRight);
+        hideClass("#skills", animationOutLeft);
+        hideClass("#edu", animationOutRight);
+        hideClass("#contact", animationOutRight);
+        hideClass("#links", animationOutRight);
+        $( "#exp").addClass(animationZoomOut).one(animationEnd, function(){
 
-            $( "#about").removeClass(animationZoomOut).hide();
+            $( "#exp").removeClass(animationZoomOut).hide();
             $( "#RWD_STYLE").hide();
-            $( "#close_button_about").show();
+            $( "#close_button_exp").show();
             $( "#certificates").show().addClass("animated fadeIn").addClass("col-xs-12 col-sm-12 col-md-12 col-lg-12").one(animationEnd, function(){
                 $( "#certificates").removeClass("animated fadeIn");
                 
-                $( "#close_button_about").unbind('click').one('click', function(){
-                    $( "#close_button_about").hide(function(){
+                $( "#close_button_exp").unbind('click').one('click', function(){
+                    $( "#close_button_exp").hide(function(){
                         $( "#certificates").addClass("animated zoomOutDown").one(animationEnd, function(){
                             if(changingCertificate === 0){
-                                $("#close_button_about").hide();
+                                $("#close_button_exp").hide();
                                 $( "#certificates").removeClass("col-xs-12 col-sm-12 col-md-12 col-lg-12").hide().removeClass("animated zoomOutDown");
-                                animateLeft("#emoticon", 1);
-                                animateLeft("#about", 1);
-                                animateRight("#name", 0);
-                                animateLeft("#skills", 0);
-                                animateRight("#edu", 0);
-                                animateRight("#contact", 0);
-                                animateRight("#links", 0);
+                                recoverClass("#emoticon", 1, animationInLeft);
+                                recoverClass("#exp", 1, animationInLeft);
+                                recoverClass("#name", 0, animationInRight);
+                                recoverClass("#skills", 0, animationInLeft);
+                                recoverClass("#edu", 0, animationInRight);
+                                recoverClass("#contact", 0, animationInRight);
+                                recoverClass("#links", 0, animationInRight);
                                 $( "#RWD_STYLE").show();
                                 console.log('inside');
                             }
